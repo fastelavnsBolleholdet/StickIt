18d02abd4fbbfd6aafb9f5b61f362139b978200e
SofieAmalie44 <sofieamaliethorlund@gmail.com>
2024-04-29 10:51:26 +0200

    Welcome screen with layout

--- app/src/main/java/com/example/stickitapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/stickitapp/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Our theme
+val Dusty = Color(0xFFE0D5C6)
+val yellowy = Color(0xFFD9A05B)
+val green = Color(0xFF79C143)
+val ash = Color(0xFF504A3E)
+val salmon = Color(0xFFFC6E51)
+val blue = Color(0xFF26A0DA)
--- app/src/main/java/com/example/stickitapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/stickitapp/ui/theme/Type.kt
@@ -2,33 +2,103 @@ package com.example.stickitapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.stickitapp.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    // Large body text
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
+    // Medium body text
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    // Small body text
+    bodySmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+    // Large title text
     titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Thin,
+        fontSize = 75.sp,
+        lineHeight = 80.sp,
+        letterSpacing = 3.sp
+    ),
+    // Medium title text
+    titleMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 50.sp,
+        lineHeight = 50.sp,
+        letterSpacing = 0.sp,
+    ),
+    // Small title text
+    titleSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 34.sp,
+        lineHeight = 34.sp,
+        letterSpacing = 0.sp
+    ),
+    // Large label text
+    labelLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 26.sp,
+        letterSpacing = 0.sp
+    ),
+    // Small label text
+    labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
-        lineHeight = 28.sp,
+        lineHeight = 22.sp,
         letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
     )
-    */
+)
+
+val karantinaFontFamily = FontFamily(
+    Font(R.font.karantina_regular, FontWeight.Normal),
+)
+
+val katibehFontFamily = FontFamily(
+    Font(R.font.katibeh_regular, FontWeight.Normal)
+)
+
+val playFairDisplayFontFamily = FontFamily(
+    Font(R.font.playfairdisplay_regular, FontWeight.Normal)
+)
+
+val jaldiFontFamily = FontFamily(
+    Font(R.font.jaldi_regular, FontWeight.Normal)
+)
+
+val jaldiBoldFontFamily = FontFamily(
+    Font(R.font.jaldi_bold, FontWeight.Normal)
 )
