acf5eca272ed70123568c47e71562030feaf35e0
natazja <natazjadahl@gmail.com>
2024-04-29 13:37:45 +0200

    Layout/Color challenge.kt update

--- app/src/main/java/com/example/stickitapp/frontPage/frontPage.kt
+++ app/src/main/java/com/example/stickitapp/frontPage/frontPage.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,8 +16,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.stickitapp.ui.theme.Dusty
+import com.example.stickitapp.ui.theme.Typography
+import com.example.stickitapp.ui.theme.ash
+import com.example.stickitapp.ui.theme.burger
 import com.example.stickitapp.ui.theme.green
+import com.example.stickitapp.ui.theme.jaldiBoldFontFamily
+import com.example.stickitapp.ui.theme.salmon
 import com.example.stickitapp.ui.theme.yellowy
 
 @Composable
@@ -25,7 +33,7 @@ fun FrontPage(navigateLoadingScreen: () -> Unit) {
     val frontPageViewModel = viewModel<frontPageViewModel>()
 
     Box (
-        Modifier.background(color = yellowy)
+        Modifier.background(color = burger)
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -34,7 +42,11 @@ fun FrontPage(navigateLoadingScreen: () -> Unit) {
         ) {
             Text(
                 text = "Welcome!",
-                modifier = Modifier.padding(top = 50.dp), color = green
+                fontFamily = jaldiBoldFontFamily,
+                style = Typography.labelSmall,
+                fontSize = 50.sp,
+                color = salmon,
+                modifier = Modifier.padding(top = 50.dp)
             )
         }
         Column (
@@ -42,13 +54,24 @@ fun FrontPage(navigateLoadingScreen: () -> Unit) {
             verticalArrangement = Arrangement.Center,
             modifier = Modifier.fillMaxSize()
         ) {
-            Text(text = "How would you like to stick it?")
+            Text(text = "How would you like to stick it?",
+                fontFamily = jaldiBoldFontFamily,
+                style = Typography.labelSmall,
+                fontSize = 20.sp,
+                color = ash)
 
             Button(
                 onClick = { frontPageViewModel.moveToLoadingScreen(navigateLoadingScreen) },
-                modifier = Modifier.padding(top = 16.dp)
+                modifier = Modifier.padding(top = 16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    Dusty)
             ) {
-                Text(text = "Click here for a sticky challenge")
+                Text(text = "Click here for a sticky challenge",
+                    fontFamily = jaldiBoldFontFamily,
+                    style = Typography.labelSmall,
+                    fontSize = 20.sp,
+                    color = salmon
+                )
             }
         }
         Column (
@@ -60,7 +83,11 @@ fun FrontPage(navigateLoadingScreen: () -> Unit) {
                 Text(
                     text = "This app works by giving you a chopstick challenge for you to complete in a given timeframe. If you are lucky you will get a rating",
                     textAlign = TextAlign.Center,
-                    modifier = Modifier
+                    modifier = Modifier,
+                    fontFamily = jaldiBoldFontFamily,
+                    style = Typography.labelSmall,
+                    fontSize = 20.sp,
+                    color = ash
                 )
             }
         }
--- app/src/main/java/com/example/stickitapp/navigation/navigation.kt
+++ app/src/main/java/com/example/stickitapp/navigation/navigation.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.stickitapp.challange.Challenge
+import com.example.stickitapp.challenge.Challenge
 import com.example.stickitapp.frontPage.FrontPage
 import com.example.stickitapp.loadingScreen.LoadingScreen
 import com.example.stickitapp.welcome.Welcome
